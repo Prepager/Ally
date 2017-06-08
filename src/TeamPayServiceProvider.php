9c283fd462dfba83fb4ccc54b98cf1cbdcf63945
Zapster <thecodingbeast@gmail.com>
2017-06-08 12:51:57 +0000

    Bootstrap

---
+++ src/TeamPayServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ZapsterStudios\TeamPay;
+
+use Illuminate\Support\ServiceProvider;
+
+class TeamPayServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
