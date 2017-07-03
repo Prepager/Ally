b25a83e9fa94264f6be1130f5efb8d2bb372bdf1
Andreas <andreas@zapsterstudios.com>
2017-07-03 16:13:27 +0000

    Apply fixes from StyleCI

--- src/Tests/Feature/TeamMemberTest.php
+++ src/Tests/Feature/TeamMemberTest.php
@@ -6,7 +6,6 @@ use App\Team;
 use App\User;
 use Laravel\Passport\Passport;
 use ZapsterStudios\TeamPay\Tests\TestCase;
-use ZapsterStudios\TeamPay\Models\TeamMember;
 
 class TeamMemberTest extends TestCase
 {
