51fe96467c3c6c310ac473720778453e45e9d0df
Zapster <thecodingbeast@gmail.com>
2017-07-07 16:36:50 +0000

    Prepared for subscribed middleware

---
+++ src/Middleware/Subscribed.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ZapsterStudios\TeamPay\Middleware;
+
+use Closure;
+
+class Subscribed
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Some check
+
+        return $next($request);
+    }
+}
